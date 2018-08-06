3efcec7f1c7d254d5ec078b718965fb7152a983f
Asgar <ihateyouthor@gmail.com>
2018-08-06 16:48:00 +0300

    Fixed some bugs

--- lib/components/app_component/app_component.dart
+++ lib/components/app_component/app_component.dart
@@ -7,10 +7,13 @@ import '../loot_list_component/loot_list_component.dart';
 @Component
 (
   selector : 'app',
-  template :'''<h1>HEH</h1>
-               <loot-list/>''',
-  directives: const [materialDirectives, CORE_DIRECTIVES],
+  template :'''<h1>{{title}}</h1>
+               <loot-list></loot-list>''',
+  directives: const [materialDirectives, CORE_DIRECTIVES, LootListComponent],
+  providers: const [LootService],
 )
 class AppComponent {
+  final title = "Looters The Game";
+
 
 }
